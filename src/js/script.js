ecb30370e74d51db3f9d3eb8d6cb7f3ab96076f8
Doomka <Do-omer@ya.ru>
2020-03-22 17:55:01 +0400

    header

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', (e)=> {
 	
+	if (document.querySelector('[data-active]')) {
+		let activators = document.querySelectorAll('[data-active]')
+		activators.forEach(function(elem) {
+			elem.addEventListener('click', function(e) {
+				elem.dataset.active.split(', ').forEach(function(target) {
+					if (target === 'this') {
+						elem.classList.toggle('_active')
+					} else if (document.querySelector(target)) {
+						document.querySelector(target).classList.toggle('_active')
+					}
+				})
+			})
+		})
+	}
+	
 })
