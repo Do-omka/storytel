2d01bae65dfd476d03496ebda5ad10137154f5a8
Doomka <Do-omer@ya.ru>
2020-04-09 03:21:30 +0400

    fixed faq blur effect

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,13 +1,19 @@
 document.addEventListener('DOMContentLoaded', (e)=> {
 	@@include('blocks/index.js')
 	
+	let toblur = document.querySelectorAll('.faq-item:last-child')
+	
 	if ("IntersectionObserver" in window) {
+		toblur.forEach((i) => {
+			i.parentElement.classList.add('_blurred')
+		})
+		
 		let blurObserver = new IntersectionObserver((entries)=> {
 			entries.forEach(function(entry) {
 				if (entry.intersectionRatio > 0) {
-					entry.target.parentElement.classList.add('_scrolled')
+					entry.target.parentElement.classList.remove('_blurred')
 				} else {
-					entry.target.parentElement.classList.remove('_scrolled')
+					entry.target.parentElement.classList.add('_blurred')
 				}
 			})
 		}, {threshold: 0.01})
@@ -17,8 +23,8 @@ document.addEventListener('DOMContentLoaded', (e)=> {
 		})
 		
 	} else {
-		document.querySelectorAll('.faq-item:last-child').forEach((faq)=> {
-			blurObserver.observe(faq.parentElement.classList.add('_scrolled'))
+		blurOnScroll.forEach((faq)=> {
+			faq.parentElement.classList.remove('_blurred')
 		})
 	}
 	
